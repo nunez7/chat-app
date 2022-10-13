2e0126c7861061f123b5b458ba590a7574530dc2
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2022-10-13 12:17:32 -0600

    Instalar la version de stomp
    Librerias para websocket (sockjs y stomjs)
    usar:
    npm install @stomp/stompjs@5.2.0

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Client } from '@stomp/stompjs';
+import * as SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-chat',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+  private client: Client;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.client = new Client();
+    //creamos la conexion
+    this.client.webSocketFactory = ()=>{
+      //El endpoint de spring /canal para enviar y recibir mensajes
+      return new SockJS("http://localhost:8883/chat-websocket");
+    }
+
+    //OBtenemos el sttaus de la conexion
+    this.client.onConnect = (frame) => {
+      console.log("Conectados "+ this.client.connected+ ": "+frame);
+    }
+    //Metodo para conectarnos
+    this.client.activate();
+
   }
 
 }
